3cb119aea9e85efce74054af72f9791a1b5c1f23
karunaket <karunaketan2077@gmail.com>
2024-11-09 01:01:06 +0530

    2nd

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,27 +1,33 @@
 function calculateBMI() { 
 
-  const weight = parseFloat(document.getElementById("weight").value); 
-  const height = parseFloat(document.getElementById("height").value) / 100;  
+const weight = parseFloat(document.getElementById("weight").value); 
+const height = parseFloat(document.getElementById("height").value) / 100;  
 
-  if (isNaN(weight) || isNaN(height) || height === 0) { 
-  document.getElementById("result").innerText = "Please enter valid values for weight and height."; 
-      return; 
-  }
+if (isNaN(weight) || isNaN(height) || height === 0) { 
+document.getElementById("result").innerText = "Please enter valid values for weight and height."; 
+    return; 
+}
 
-  
   const bmi = weight / (height * height); 
-  let category = ""; 
+let category = ""; 
 
-  if (bmi < 18.5) { 
-      category = "Underweight"; 
-  } else if (bmi < 24.9) { 
-      category = "Normal weight"; 
-  } else if (bmi < 29.9) { 
-      category = "Overweight"; 
-  } else { 
-      category = "Obesity"; 
-  } 
+if (bmi < 18.5)
+{ 
+    category = "Underweight"; 
+}
+else if (bmi < 24.9)
+{ 
+    category = "Normal weight"; 
+}
+else if (bmi < 29.9)
+{ 
+    category = "Overweight"; 
+}
+else
+{ 
+    category = "Obesity"; 
+} 
 
-  document.getElementById("result").innerText = `Your BMI is 
+document.getElementById("result").innerText = `Your BMI is 
 ${bmi.toFixed(2)} (${category})`; 
 }
